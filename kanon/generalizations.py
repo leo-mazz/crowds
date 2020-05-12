36bb385d6aa6a0ffdb1ee81d3fbec59bc824bad1
Leonardo Mazzone <leo@mazzone.space>
2020-05-12 12:08:26 +0100

    Complete tests for generalizations

--- kanon/generalizations.py
+++ kanon/generalizations.py
@@ -1,13 +1,26 @@
+def _wrap_level(level):
+    def wrapped_level(value):
+        res = level(value)
+        if res is None:
+            raise ValueError('Generalization step provided has no coverage for this value')
+
+        return res
+    
+    return wrapped_level
+
 class GenRule():
     def __init__(self, levels):
         id_fn = lambda x: x
         none_fn = lambda x: None
-        self._levels = [id_fn] + levels + [none_fn]
+        custom_levels = [_wrap_level(lvl) for lvl in levels]
+        self._levels = [id_fn] + custom_levels + [none_fn]
         self.max_level = len(self._levels) - 1
 
     def _verify_level(self, level):
-        if level < 0 or level > self.max_level:
-            raise ValueError('Not enough generalization steps available with this rule')
+        if not isinstance(level, int) or level < 0:
+            raise ValueError('Level must be a positive integer')
+        if level > self.max_level:
+            raise ValueError('Not enough generalization steps available to this rule')
     
     def apply(self, value, level):
         self._verify_level(level)
@@ -16,3 +29,5 @@ class GenRule():
     def level(self, lvl):
         self._verify_level(lvl)
         return self._levels[lvl]
+
+__all__ = ['GenRule']
--- tests/test_generalizations.py
+++ tests/test_generalizations.py
@@ -1,5 +1,46 @@
 from kanon.generalizations import GenRule
 
+import pytest
+
+
+def custom_level_one(value):
+    return value % 3 # maps everything to 0, 1, 2
+
+def custom_level_two(value):
+    return value % 2 # maps 0 and 2 to 0 and 1 to 1
+
+def incomplete_level(value):
+    if value % 3 == 0:
+        return 0
+    if value % 3 == 1:
+        return 1
+
 def test_one_level_genrule():
     rule = GenRule([])
+    assert rule.apply('any', 0) == 'any'
     assert rule.apply('any', 1) is None
+
+
+def test_custom_levels():
+    rule = GenRule([custom_level_one, custom_level_two])
+    assert rule.apply(11, 1) == 2
+    assert rule.apply(2, 2) == 0
+
+def test_verify_level():
+    rule = GenRule([custom_level_one, custom_level_two])
+    with pytest.raises(ValueError):
+        rule.apply(8, 'level1')
+    with pytest.raises(ValueError):
+        rule.apply(8, -2)
+    with pytest.raises(ValueError):
+        rule.apply(8, 4) # 0 is identity, 1 and 2 are custom, 3 is map to None
+
+def test_missing_coverage():
+    rule = GenRule([incomplete_level])
+    with pytest.raises(ValueError):
+        rule.apply(8, 1)
+
+def test_apply_level_equivalent():
+    rule = GenRule([custom_level_one])
+    assert rule.apply(12, 1) == rule.level(1)(12) != None
+    assert rule.apply(13, 1) == rule.level(1)(13) != None
